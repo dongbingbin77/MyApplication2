37d91ceb29e5229fbb232d67cad9f0d5036a9240
dongbingbin77 <56656174@qq.com>
2017-03-06 14:53:30 +0800

    develop1 main2_2 (cherry picked from commit 8046585)

--- app/src/main/java/com/example/asus/myapplication2/Main2Activity.java
+++ app/src/main/java/com/example/asus/myapplication2/Main2Activity.java
@@ -10,5 +10,6 @@ public class Main2Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         //main2
+        //main2_2
     }
 }
